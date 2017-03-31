1fc51ad1bf8b7ca2eba154aa7d2078d2cf796624
Tom Norton <thomas.norton@cyberbotics.com>
2017-03-31 10:40:26 +0200

    remove bogus files

--- spec/PROTO-Webots-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import protoWebotsView from '../lib/PROTO-Webots-view';
-
-describe('protoWebotsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
